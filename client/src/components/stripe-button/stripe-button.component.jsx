36f50541ebb79a834a023848d071d36bbf801fb0
framptonb <bgr7597@hotmail.com>
2021-03-01 09:10:09 -0400

    modified stripe pub key and changed red test for cc payment page

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
+  const publishableKey = 'pk_test_51ILrc3LalVIEXCU4Zz98c1GpxaJhugoNKwR3jUeC8GnyOIKHqQ5GYQbjZXnDOICOYVPndfjuVe420Q9T9OEnhVVe00aTRKwkQK';
 
   const onToken = token => {
     axios({
